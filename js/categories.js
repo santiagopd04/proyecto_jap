31218855a7cd319d20eef531aa9103c92c18400f
santiagopd04 <hebersantiagop@gmail.com>
2021-08-21 22:31:32 -0300

    Productos
    Agregue el fetch que trae la lista de productos

--- js/categories.js
+++ js/categories.js
@@ -79,9 +79,9 @@ function sortAndShowCategories(sortCriteria, categoriesArray){
     showCategoriesList();
 }
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+/*Función que se ejecuta una vez que se haya lanzado el evento de
+que el documento se encuentra cargado, es decir, se encuentran todos los
+elementos HTML presentes.*/
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(CATEGORIES_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
--- js/products.js
+++ js/products.js
@@ -1,6 +1,25 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    
+    fetch(PRODUCTS_URL)
+        .then(data => data.json())
+        .then(products => {
+            for(let i = 0; i < products.length; i++){
+                let productInfo = products[i];
+                document.getElementById("products-list").innerHTML += `
+                <a href="product-info.html" class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + productInfo.imgSrc + `" alt="`+ productInfo.description +   `" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ productInfo.name +`</h4>
+                                <small class="text-muted">` + productInfo.currency +` `+ productInfo.cost + `</small>
+                            </div>
+                            <p class="mb-1">` + productInfo.description + `</p>
+                        </div>
+                    </div>
+                </a>`
+            }
+        });
 });
