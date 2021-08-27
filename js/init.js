0a1308289ad123173d6b42cdd939fbfa8a85ec90
santiagopd04 <hebersantiagop@gmail.com>
2021-08-26 21:28:48 -0300

    Mostrar usuario. Entrega 2
    Agregue el campo que muestra el usuario en la barra de navegacion

--- js/init.js
+++ js/init.js
@@ -44,4 +44,6 @@ var getJSONData = function(url){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+  let usuario = localStorage.getItem('Usuario');
+  document.getElementById("mostrarusuario").innerHTML = "Usuario: " + usuario
 });
--- js/login.js
+++ js/login.js
@@ -2,5 +2,12 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    
+    document.getElementById(id="loginbtn").addEventListener("click", function(e){
+        let mail = document.getElementById("inputEmail").value;
+        let fin = mail.indexOf("@");
+        let usuario = mail.substring(0, fin);
+        localStorage.setItem("Usuario", usuario);
 
+    })
 });
